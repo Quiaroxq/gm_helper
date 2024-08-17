507059e6f2dfb1aaa3ba26c75a34008e8413a491
unknown <xmaster.vlad2003@gmail.com>
2024-08-17 21:39:57 +0800

    gmh

---
+++ app.py
@@ -0,0 +1,217 @@
+from flask import Flask, render_template, request
+import requests
+import json
+from bs4 import BeautifulSoup
+import threading
+import time
+import uuid
+
+app = Flask(__name__)
+
+# Учетные данные для получения токена
+auth = "ZTAxZWE5ODMtMzViNi00MGMxLTk0ODktYjFiZTU0NWJjNzc3OmM4ZmU3NzYzLWEwYTYtNGM1Yy1iNjAwLTA2OGI2ZDU1ZDAwNg=="
+client_id = "e01ea983-35b6-40c1-9489-b1be545bc777"
+secret = "c8fe7763-a0a6-4c5c-b600-068b6d55d006"
+giga_token = ""
+# Функция для получения токена
+def get_token(auth_token, scope='GIGACHAT_API_PERS'):
+    rq_uid = str(uuid.uuid4())
+    url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'RqUID': rq_uid,
+        'Authorization': f'Basic {auth_token}'
+    }
+
+    payload = {
+        'scope': scope
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=payload, verify=False)
+        return response
+    except requests.RequestException as e:
+        print(f'Ошибка: {str(e)}')
+        return None
+# Функция для обновления токена каждые 30 минут
+def update_token():
+    global giga_token
+    # Инициализация токена при старте
+    response = get_token(auth)
+    if response and response.status_code == 200:
+        giga_token = response.json()['access_token']
+        print("Токен обновлен:")
+    else:
+        print("Не удалось обновить токен при старте.")
+
+    while True:
+        time.sleep(1800) 
+        response = get_token(auth)
+        if response and response.status_code == 200:
+            giga_token = response.json()['access_token']
+            print("Токен обновлен:", giga_token)
+        else:
+            print("Не удалось обновить токен.")
+
+# Запускаем поток для обновления токена
+token_thread = threading.Thread(target=update_token)
+token_thread.daemon = True
+token_thread.start()
+
+# Загрузка html-шаблона
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Обработка запроса на генерацию текста
+@app.route('/generate_text', methods=['POST'])
+def generate_text():
+    user_message = request.form['user_message']
+    chat_response = get_chat_completion(giga_token, user_message)
+    
+    if chat_response:
+        answer_content = chat_response['choices'][0]['message']['content']
+    else:
+        answer_content = "Не удалось получить ответ."
+
+    return render_template('index.html', answer=answer_content)
+
+
+#Обработка запроса на генерацию изображения
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    user_message = request.form['image_message']
+    chat_response = get_chat_image(giga_token, user_message) # сначала получаем расписанный промпт от текстового помощника
+
+    if chat_response:
+        answer_content = chat_response['choices'][0]['message']['content']
+    else:
+        answer_content = "Не удалось получить ответ."
+
+    response_img_tag = send_chat_request(giga_token, answer_content) # а здесь полученный промпт просим изобразить
+
+    if response_img_tag:
+        soup = BeautifulSoup(response_img_tag, 'html.parser')
+        img_src = soup.img['src']
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {giga_token}',
+        }
+
+        response = requests.get(f'https://gigachat.devices.sberbank.ru/api/v1/files/{img_src}/content', headers=headers, verify=False)
+
+        with open('static/image.jpg', 'wb') as f:
+            f.write(response.content)
+
+        return render_template('index.html', image_generated=True)
+    else:
+        return render_template('index.html', image_generated=False)
+
+
+# функция для отправки post запроса для генерации изображения
+def send_chat_request(giga_token, user_message):
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {giga_token}',
+    }
+
+    payload = {
+        "model": "GigaChat:latest",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Ты - Василий Кандинский"
+            },
+            {
+                "role": "user",
+                "content": f"Нарисуй {user_message}"
+            }
+        ],
+        "function_call": "auto",
+        "temperature": 0.7
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(payload), verify=False)
+        return response.json()["choices"][0]["message"]["content"]
+    except requests.RequestException as e:
+        print(f"Произошла ошибка: {str(e)}")
+        return None
+
+
+# функция для отправки post запроса для ответа на вопросы по правилам 
+def get_chat_completion(auth_token, user_message):
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    payload = json.dumps({
+        "model": "GigaChat",
+        "messages": [
+            {
+                "role": "user",
+                "content": f"Расскажи про правило Dungeouns and Dragons в котором говорится о том{user_message}"
+            }
+        ],
+        "temperature": 1,
+        "top_p": 0.1,
+        "n": 1,
+        "stream": False,
+        "max_tokens": 512,
+        "repetition_penalty": 1,
+        "update_interval": 0
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {auth_token}'
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=payload, verify=False)
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Произошла ошибка: {str(e)}")
+        return None
+    
+# функция для отправки post запроса на генерацию промпта для изображения
+def get_chat_image(auth_token, user_message):
+    url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+    payload = json.dumps({
+        "model": "GigaChat",
+        "messages": [
+            {
+                "role": "user",
+                "content": f"Опипши словами {user_message}, в пределах 40 слов"
+            }
+        ],
+        "temperature": 1,
+        "top_p": 0.1,
+        "n": 1,
+        "stream": False,
+        "max_tokens": 512,
+        "repetition_penalty": 1,
+        "update_interval": 0
+    })
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {auth_token}'
+    }
+
+    try:
+        response = requests.post(url, headers=headers, data=payload, verify=False)
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Произошла ошибка: {str(e)}")
+        return None
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
---
+++ bot.py
@@ -0,0 +1,33 @@
+import nest_asyncio
+import asyncio
+import logging
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+
+nest_asyncio.apply()  # Активируем nest_asyncio
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+TELEGRAM_BOT_TOKEN = '6684660741:AAEXXWt4rOVe_WOBwXoBvlHdF3fEWJPoUoA'  # Замените на свой токен
+WEB_APP_URL = 'https://working-longhorn-suddenly.ngrok-free.app'
+IMAGE_URL = 'https://ibb.co/vkSpBCf'
+
+async def start(update: Update, context: CallbackContext) -> None:
+    # Создаем кнопку с встроенной ссылкой
+    keyboard = [
+        [InlineKeyboardButton("Let our journey begin", url=WEB_APP_URL)]
+    ]
+    
+    # Создаем разметку для кнопки
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    # Отправляем изображение с текстом и кнопкой
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo=IMAGE_URL, caption='Hello traveler! To contact this wizard, click on the sign below', reply_markup=reply_markup)
+
+async def main() -> None:
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    await application.run_polling()
+
+if __name__ == '__main__':
+    asyncio.run(main())
